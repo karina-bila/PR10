b29bfde2d48a13bbde3a3c336c55c73b86d1ed47
karina-bila <karinaslusar246@gmail.com>
2023-10-25 23:48:54 +0300

    лабораторна 17-18,1

--- MyForm.h
+++ MyForm.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 namespace Project1 {
 
@@ -51,6 +51,14 @@ namespace Project1 {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel1;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::ComboBox^ comboBox3;
+	private: System::Windows::Forms::ComboBox^ comboBox4;
+	private: System::Windows::Forms::CheckedListBox^ checkedListBox1;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::LinkLabel^ linkLabel1;
+	private: System::Windows::Forms::ListBox^ listBox1;
 
 
 
@@ -58,8 +66,7 @@ namespace Project1 {
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
-
+		System::ComponentModel::Container^ components;
 #pragma region Windows Form Designer generated code
 		/// <summary>
 		/// Required method for Designer support - do not modify
@@ -84,9 +91,16 @@ namespace Project1 {
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->flowLayoutPanel1 = (gcnew System::Windows::Forms::FlowLayoutPanel());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->comboBox3 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox4 = (gcnew System::Windows::Forms::ComboBox());
+			this->checkedListBox1 = (gcnew System::Windows::Forms::CheckedListBox());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->linkLabel1 = (gcnew System::Windows::Forms::LinkLabel());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
-			this->flowLayoutPanel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -98,7 +112,7 @@ namespace Project1 {
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(246, 25);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"�������                             ";
+			this->label1->Text = L"Прізвище                             ";
 			// 
 			// label2
 			// 
@@ -109,7 +123,7 @@ namespace Project1 {
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(49, 25);
 			this->label2->TabIndex = 1;
-			this->label2->Text = L"��\'�";
+			this->label2->Text = L"Ім\'я";
 			// 
 			// label3
 			// 
@@ -120,18 +134,19 @@ namespace Project1 {
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(182, 25);
 			this->label3->TabIndex = 2;
-			this->label3->Text = L"���� ����������";
+			this->label3->Text = L"Дата народження";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
+
 			this->label4->Location = System::Drawing::Point(12, 215);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(72, 25);
 			this->label4->TabIndex = 3;
-			this->label4->Text = L"�����";
+			this->label4->Text = L"Країна";
 			// 
 			// label5
 			// 
@@ -142,7 +157,7 @@ namespace Project1 {
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(79, 25);
 			this->label5->TabIndex = 4;
-			this->label5->Text = L"������";
+			this->label5->Text = L"Адреса";
 			// 
 			// textBox1
 			// 
@@ -170,7 +185,10 @@ namespace Project1 {
 			// comboBox1
 			// 
 			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"������", L"������" });
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(5) {
+				L"Україна", L"США", L"Велика Британія", L"Франція",
+					L"Німеччина"
+			});
 			this->comboBox1->Location = System::Drawing::Point(104, 212);
 			this->comboBox1->Name = L"comboBox1";
 			this->comboBox1->Size = System::Drawing::Size(121, 28);
@@ -179,12 +197,12 @@ namespace Project1 {
 			// comboBox2
 			// 
 			this->comboBox2->FormattingEnabled = true;
-			this->comboBox2->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"������", L"������" });
+			this->comboBox2->Items->AddRange(gcnew cli::array< System::Object^  >(4) { L"1", L"2", L"3", L"4" });
 			this->comboBox2->Location = System::Drawing::Point(33, 428);
 			this->comboBox2->Name = L"comboBox2";
 			this->comboBox2->Size = System::Drawing::Size(121, 28);
 			this->comboBox2->TabIndex = 9;
-			this->comboBox2->Text = L"����";
+			this->comboBox2->Text = L"Курс";
 			// 
 			// textBox3
 			// 
@@ -198,9 +216,9 @@ namespace Project1 {
 			// pictureBox1
 			// 
 			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(391, 37);
+			this->pictureBox1->Location = System::Drawing::Point(791, 36);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(309, 204);
+			this->pictureBox1->Size = System::Drawing::Size(305, 204);
 			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
 			this->pictureBox1->TabIndex = 11;
 			this->pictureBox1->TabStop = false;
@@ -209,23 +227,24 @@ namespace Project1 {
 			// pictureBox2
 			// 
 			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
-			this->pictureBox2->Location = System::Drawing::Point(660, 36);
+			this->pictureBox2->Location = System::Drawing::Point(1060, 35);
 			this->pictureBox2->Name = L"pictureBox2";
-			this->pictureBox2->Size = System::Drawing::Size(309, 204);
+			this->pictureBox2->Size = System::Drawing::Size(305, 204);
 			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
 			this->pictureBox2->TabIndex = 12;
 			this->pictureBox2->TabStop = false;
 			this->pictureBox2->Visible = false;
 			// 
 			// button1
-			// 
+			//
+
 			this->button1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
 				static_cast<System::Int32>(static_cast<System::Byte>(128)));
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button1->Location = System::Drawing::Point(3, 3);
+			this->button1->Location = System::Drawing::Point(1083, 266);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(129, 66);
+			this->button1->Size = System::Drawing::Size(125, 66);
 			this->button1->TabIndex = 13;
 			this->button1->Text = L"<=";
 			this->button1->UseVisualStyleBackColor = false;
@@ -234,9 +253,7 @@ namespace Project1 {
 			// flowLayoutPanel1
 			// 
 			this->flowLayoutPanel1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
-			this->flowLayoutPanel1->Controls->Add(this->button1);
-			this->flowLayoutPanel1->Controls->Add(this->button2);
-			this->flowLayoutPanel1->Location = System::Drawing::Point(593, 289);
+			this->flowLayoutPanel1->Location = System::Drawing::Point(1073, 260);
 			this->flowLayoutPanel1->Name = L"flowLayoutPanel1";
 			this->flowLayoutPanel1->Size = System::Drawing::Size(279, 72);
 			this->flowLayoutPanel1->TabIndex = 14;
@@ -247,20 +264,123 @@ namespace Project1 {
 				static_cast<System::Int32>(static_cast<System::Byte>(192)));
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button2->Location = System::Drawing::Point(138, 3);
+			this->button2->Location = System::Drawing::Point(1214, 266);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(129, 66);
+			this->button2->Size = System::Drawing::Size(125, 66);
 			this->button2->TabIndex = 15;
 			this->button2->Text = L"=>";
 			this->button2->UseVisualStyleBackColor = false;
 			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
+			// comboBox3
+			// 
+			this->comboBox3->FormattingEnabled = true;
+			this->comboBox3->Items->AddRange(gcnew cli::array< System::Object^  >(8) {
+				L"1СОМ", L"2СОМ", L"3СОМ", L"4СОМ", L"1СОФ", L"2СОЇ",
+					L"3М", L"4МС"
+			});
+			this->comboBox3->Location = System::Drawing::Point(185, 428);
+			this->comboBox3->Name = L"comboBox3";
+			this->comboBox3->Size = System::Drawing::Size(121, 28);
+			this->comboBox3->TabIndex = 16;
+			this->comboBox3->Text = L"Група";
+			// 
+			// comboBox4
+			// 
+			this->comboBox4->FormattingEnabled = true;
+			this->comboBox4->Items->AddRange(gcnew cli::array< System::Object^  >(3) {
+				L"Середня освіта. Математика", L"Середня освіта. Фізика",
+					L"Середня освіта. Інформатика"
+			});
+			this->comboBox4->Location = System::Drawing::Point(349, 428);
+			this->comboBox4->Name = L"comboBox4";
+			this->comboBox4->Size = System::Drawing::Size(265, 28);
+			this->comboBox4->TabIndex = 17;
+			this->comboBox4->Text = L"Спеціальність";
+			// 
+			// checkedListBox1
+			// 
+			this->checkedListBox1->FormattingEnabled = true;
+			this->checkedListBox1->Items->AddRange(gcnew cli::array< System::Object^  >(5) {
+				L"Програмування", L"Вища математика", L"Історія",
+					L"Інформатика", L"Англійська мова"
+			});
+			this->checkedListBox1->Location = System::Drawing::Point(33, 501);
+			this->checkedListBox1->Name = L"checkedListBox1";
+			this->checkedListBox1->Size = System::Drawing::Size(273, 326);
+			this->checkedListBox1->TabIndex = 18;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(104, 855);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(151, 47);
+			this->button3->TabIndex = 19;
+			this->button3->Text = L"Я вивчаю";
+			this->button3->UseVisualStyleBackColor = true;
+
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(945, 855);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(151, 47);
+			this->button4->TabIndex = 20;
+			this->button4->Text = L"Про мене";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(945, 501);
+			this->textBox4->Multiline = true;
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
+			this->textBox4->Size = System::Drawing::Size(324, 326);
+			this->textBox4->TabIndex = 21;
+			// 
+			// linkLabel1
+			// 
+			this->linkLabel1->AutoSize = true;
+			this->linkLabel1->Cursor = System::Windows::Forms::Cursors::IBeam;
+			this->linkLabel1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->linkLabel1->ForeColor = System::Drawing::Color::Blue;
+			this->linkLabel1->Location = System::Drawing::Point(43, 944);
+			this->linkLabel1->Name = L"linkLabel1";
+			this->linkLabel1->Size = System::Drawing::Size(31, 46);
+			this->linkLabel1->TabIndex = 22;
+			this->linkLabel1->TabStop = true;
+			this->linkLabel1->Text = L"f";
+			this->linkLabel1->VisitedLinkColor = System::Drawing::Color::Blue;
+			this->linkLabel1->LinkClicked += gcnew System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &MyForm::linkLabel1_LinkClicked);
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 20;
+			this->listBox1->Location = System::Drawing::Point(363, 501);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(251, 324);
+			this->listBox1->TabIndex = 23;
+			this->listBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm::listBox1_SelectedIndexChanged);
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(981, 616);
-			this->Controls->Add(this->flowLayoutPanel1);
+			this->ClientSize = System::Drawing::Size(1380, 1037);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->linkLabel1);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->checkedListBox1);
+			this->Controls->Add(this->comboBox4);
+			this->Controls->Add(this->comboBox3);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->comboBox2);
@@ -274,11 +394,11 @@ namespace Project1 {
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->pictureBox2);
+			this->Controls->Add(this->flowLayoutPanel1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
-			this->flowLayoutPanel1->ResumeLayout(false);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -289,11 +409,33 @@ namespace Project1 {
 		pictureBox2->Visible = false;
 	}
 
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		pictureBox2->Visible = true;
+		pictureBox1->Visible = false;
+	}
 
-private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-	pictureBox2->Visible = true;
-	pictureBox1->Visible = false;
-}
-
-};
+	private: System::Void linkLabel1_LinkClicked(System::Object^ sender, System::Windows::Forms::LinkLabelLinkClickedEventArgs^ e) {
+		System::Diagnostics::Process::Start("https://www.facebook.com/kkarina.bila/");
+	}
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+		for (int i = 0; i < checkedListBox1->CheckedItems->Count; i++)
+		{
+			listBox1->Items->Add(checkedListBox1->CheckedItems[i]);
+		}
+	}
+	private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+		textBox4->Text = "Прiзвище: " + textBox1->Text + "\r\n";
+		textBox4->Text += "IM'я : " + textBox2->Text + "\r\n";
+		textBox4->Text += "Дата народження:" + maskedTextBox1->Text + "\r\n";;
+		textBox4->Text += "Aдpeca:" + textBox3->Text + "\r\n";
+		textBox4->Text += "Kрaïнa: " + comboBox1->Text + "\r\n";
+		textBox4->Text += "Kypc: " + comboBox2->Text + "\r\n";
+		textBox4->Text += "rpyna: "
+			+ comboBox3->Text + "\r\n";
+		textBox4->Text += "Cneuiальнiсть:" + comboBox4->Text + "\r\n";
+	}
+	private: System::Void listBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+		listBox1->Items->Remove(listBox1->SelectedItem);
+	}
+	};
 }
